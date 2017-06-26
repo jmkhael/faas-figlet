cb6b7d6d5041d89217f10b0d02c4ec5341c3e094
Johnny Mkhael <johnny.mkhael@gmail.com>
2017-06-26 14:48:07 +0200

    localhost and defaults stack names

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker service rm figlet
+docker service rm func_figlet
 
 curl localhost:8080/system/functions -d '
 {"service": "func_figlet", "image": "jmkhael/faas-figlet", "envProcess": "figlet", "network": "func_functions"}'
